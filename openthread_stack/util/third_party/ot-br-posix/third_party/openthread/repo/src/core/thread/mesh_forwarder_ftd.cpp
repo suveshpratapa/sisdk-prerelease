0cf364188354e7f35c4eea4d2d121fe524b33c2f
Suvesh Pratapa <suvesh.pratapa@silabs.com>
2026-02-12 01:23:07 -0500

    Silicon Labs sisdk-2025.12 SDK with Project Iris support

--- openthread_stack/util/third_party/ot-br-posix/third_party/openthread/repo/src/core/thread/mesh_forwarder_ftd.cpp
+++ openthread_stack/util/third_party/ot-br-posix/third_party/openthread/repo/src/core/thread/mesh_forwarder_ftd.cpp
@@ -48,6 +48,20 @@ void MeshForwarder::SendMessage(OwnedPtr<Message> aMessagePtr)
     message.SetOffset(0);
     message.SetDatagramTag(0);
     message.SetTimestampToNow();
+
+#if FEATURE_TIMESYNCSERVICE_ENABLE
+    if (ot::TimeSyncService::TimeSyncService::isTimeSyncPacket(message))
+    {
+        message.SetTxTimestampEnabled(true);
+        LogDebg("SendMessage:: Handling Time Sync Packet, frame_rx_time = %llu, time stamp enabled %d", 
+            message.GetRadioTime(), message.IsTxTimestampEnabled());
+
+        #if OPENTHREAD_CONFIG_MULTI_RADIO
+            message.SetRadioType(ot::Mac::kRadioTypeIeee802154);
+        #endif
+    }
+#endif
+
     mSendQueue.Enqueue(message);
 
     switch (message.GetType())
